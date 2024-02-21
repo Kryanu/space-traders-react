8d4f739eb2a8423ddd9701c6ab1bcc22524e83ea
Kryanu <82233879+Kryanu@users.noreply.github.com>
2024-02-21 14:36:10 +0100

    Added Faction choosing to register

--- src/components/organisms/Start/start.jsx
+++ src/components/organisms/Start/start.jsx
@@ -4,23 +4,32 @@ import { registerHandle } from './logic';
 import '../../../App.css';
 
 export default function Start() {
-  const { handle, changeHandle } = userDataStore();
+  const { handle, faction, changeHandle, changeFaction } = userDataStore();
   const { gameState, updateGame } = gameDataStore();
-
   return (
     <>
       <h1 className='mb-8'>Space Traders</h1>
       <div className='flex flex-col space-y-4'>
-        <TextField
-          variant='outlined'
-          label='Agent Handler'
-          onChange={(e) => {
-            changeHandle(e.target.value);
-          }}
-        ></TextField>
+        <div className='flex space-x-4'>
+          <TextField
+            variant='outlined'
+            label='Agent Handler'
+            onChange={(e) => {
+              changeHandle(e.target.value);
+            }}
+          />
+          <TextField
+            variant='outlined'
+            label='Faction'
+            onChange={(e) => {
+              changeFaction(e.target.value);
+            }}
+          />
+        </div>
+
         <Button
           onClick={async () =>
-            updateGame(await registerHandle(handle, 'COSMIC'))
+            updateGame(await registerHandle(handle, faction))
           }
           variant='contained'
           sx={{
--- src/stores/userData.js
+++ src/stores/userData.js
@@ -2,5 +2,7 @@ import { create } from 'zustand';
 
 export const userDataStore = create((set) => ({
   handle: undefined,
+  faction: undefined,
   changeHandle: (newHandle) => set(() => ({ handle: newHandle })),
+  changeFaction: (newFaction) => set(() => ({ faction: newFaction }))
 }));
