cb8f6275df2241231c3f4472c694a77e00cba69d
Kryanu <82233879+Kryanu@users.noreply.github.com>
2024-02-23 12:44:44 +0100

    Created first draft of contract details

--- src/components/pages/ContractDetails/logic.js
+++ src/components/pages/ContractDetails/logic.js
@@ -1,10 +1,23 @@
-import { API } from "../../../api/service";
-import { filterRecordByHandle } from "../../../api/pocketbase";
-export const retrieveContractDetails = async (token, contractId, setDetails) => {
-    const data = await API.getContract(token, contractId);
-    setDetails(data)
-}
-export const retrieveToken = async (handle, changeToken) => {
-    const data = await filterRecordByHandle(handle);
-    changeToken(data?.items[0]?.token);
-  };
+import { API } from '../../../api/service';
+
+export const retrieveContractDetails = async (token, contractId, hooks) => {
+  const { setDetails, setIsAccepted, setIsLoading } = hooks;
+  setIsLoading(true);
+  const data = await API.getContract(token, contractId);
+  setDetails(data);
+  setIsLoading(false);
+  if (data?.accepted) {
+    setIsAccepted(true);
+  }
+};
+
+export const acceptContract = async ({ token, contractId, setIsAccepted }) => {
+  try {
+    await API.acceptContract(token, contractId);
+    setIsAccepted(true);
+  } catch (ex) {
+    if (setIsAccepted) {
+      setIsAccepted(false);
+    }
+  }
+};
