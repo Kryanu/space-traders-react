d7d34a3c156f5f8b446a151610915cc787594950
Kryanu <82233879+Kryanu@users.noreply.github.com>
2024-02-28 01:56:43 +0100

    Added endpoints

--- src/api/service.js
+++ src/api/service.js
@@ -68,7 +68,57 @@ export const API = {
           headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${token}`,
-            Accept: 'application/json'
+            Accept: 'application/json',
+          },
+        }
+      );
+    } catch (ex) {
+      throw new Error(ex);
+    }
+  },
+  listWaypoints: async (token, system, traits) => {
+    let url = `https://api.spacetraders.io/v2/systems/${system}/waypoints`;
+    if (traits) {
+      url = `https://api.spacetraders.io/v2/systems/${system}/waypoints?traits=${traits}`;
+    }
+    try {
+      const res = await axios.get(url, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return res?.data?.data || res.data || res;
+    } catch (ex) {
+      throw new Error(ex);
+    }
+  },
+  getShipyard: async (token, system, waypoint) => {
+    try {
+      const res = await axios.get(
+        `https://api.spacetraders.io/v2/systems/${system}/waypoints/${waypoint}/shipyard`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return res?.data?.data || res.data || res;
+    } catch (ex) {
+      throw new Error(ex);
+    }
+  },
+  purchaseShip: async (token, shipType, waypointSymbol) => {
+    try {
+      const res = await axios.post(
+        `https://api.spacetraders.io/v2/my/ships`,
+        { shipType, waypointSymbol },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/json',
           },
         }
       );
