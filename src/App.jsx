048d2311cb8209ec041cd4b3e78880cfda58c6ab
Kryanu <82233879+Kryanu@users.noreply.github.com>
2024-03-06 21:50:58 +0100

    refactored router

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,9 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import './App.css';
-import { Start } from './components/organisms';
-import {
-  PlayerConsole,
-  ContractDetails,
-  ShipShop,
-  AsteroidMining,
-  Markets,
-} from './components/pages';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { useToken } from './hooks';
-import CountDown from './components/atoms/Countdown';
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Start />,
-  },
-  {
-    path: '/console',
-    element: <PlayerConsole />,
-  },
-  {
-    path: '/console/:contractId',
-    element: <ContractDetails />,
-  },
-  {
-    path: '/console/ship-shop',
-    element: <ShipShop />,
-  },
-  {
-    path: '/console/astroid-mining',
-    element: <AsteroidMining />,
-  },
-  {
-    path: '/console/market',
-    element: <Markets />,
-  },
-  {
-    path: '/kitchen-sink',
-    element: (
-      <CountDown
-        arrival={'2024-03-04T20:44:44.796Z'}
-        departure={'2024-03-04T18:41:34.796Z'}
-      />
-    ),
-  },
-]);
-
+import { router } from './router/router.jsx';
+import { RouterProvider } from 'react-router-dom';
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
---
+++ src/router/router.jsx
@@ -0,0 +1,46 @@
+import { Start } from '../components/organisms';
+import {
+  PlayerConsole,
+  ContractDetails,
+  ShipShop,
+  AsteroidMining,
+  Markets,
+} from '../components/pages';
+import CountDown from '../components/atoms/Countdown';
+import { createBrowserRouter } from 'react-router-dom';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Start />,
+  },
+  {
+    path: '/console',
+    element: <PlayerConsole />,
+  },
+  {
+    path: '/console/:contractId',
+    element: <ContractDetails />,
+  },
+  {
+    path: '/console/ship-shop',
+    element: <ShipShop />,
+  },
+  {
+    path: '/console/astroid-mining',
+    element: <AsteroidMining />,
+  },
+  {
+    path: '/console/market',
+    element: <Markets />,
+  },
+  {
+    path: '/kitchen-sink',
+    element: (
+      <CountDown
+        arrival={'2024-03-04T20:44:44.796Z'}
+        departure={'2024-03-04T18:41:34.796Z'}
+      />
+    ),
+  },
+]);
