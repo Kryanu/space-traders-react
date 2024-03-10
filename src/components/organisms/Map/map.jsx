55eeec778f2c6150dd0fafeafe58f44f58352bb2
Kryanu <82233879+Kryanu@users.noreply.github.com>
2024-03-10 14:00:39 +0100

    Added modal and star chart navigation

--- src/components/organisms/Map/map.jsx
+++ src/components/organisms/Map/map.jsx
@@ -1,12 +1,16 @@
-import { List, ListItem, ListItemText, Typography } from '@mui/material';
 import {
-  ScatterChart,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Scatter,
-} from 'recharts';
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from '@mui/material';
+import { ScatterChart, XAxis, YAxis, Tooltip, Scatter } from 'recharts';
 import { isValidArray } from '../../../hooks';
+import { Modal } from '../';
+import { useContext, useState } from 'react';
+import { GameContext, TokenContext } from '../../../context';
+import { API } from '../../../api/service';
 const customToolTip = (props) => {
   const { payload } = props;
   if (!isValidArray(payload)) {
@@ -27,44 +31,85 @@ const customToolTip = (props) => {
   );
 };
 
+const navigateShip = async ({ token, shipSymbol, waypointSymbol }) => {
+  if (!token) {
+    throw new Error('Token not found');
+  }
+  try {
+    await API.navigateShip(token, shipSymbol, waypointSymbol);
+  } catch {}
+};
+
 export default function Map(props) {
   const { data, title } = props;
+  const { ships } = useContext(GameContext);
+  const { token } = useContext(TokenContext);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedWaypoint, setSelectedWaypoint] = useState(undefined);
+  const openModal = (waypoint) => {
+    setSelectedWaypoint(waypoint);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => setIsModalOpen(false);
+
   if (!data) {
     return <></>;
   }
 
   return (
-    <div className='flex flex-col items-start'>
-      <Typography variant='h4' color={'#32C832'}>
-        {`${title}`}
-      </Typography>
-      <ScatterChart width={700} height={700}>
-        <XAxis
-          dataKey='x'
-          tick={true}
-          hide={true}
-          axisLine={false}
-          type='number'
-          name='X'
-        />
-        <YAxis
-          dataKey='y'
-          tick={true}
-          hide={true}
-          axisLine={false}
-          type='number'
-          name='Y'
-        />
-        <Tooltip content={customToolTip} />
-        <Scatter
-          name='A school'
-          data={data}
-          fill='#32C832'
-          onClick={(e) => {
-            console.log(e);
-          }}
-        />
-      </ScatterChart>
-    </div>
+    <>
+      <div className='flex flex-col items-start'>
+        <Typography variant='h4' color={'#32C832'}>
+          {`${title}`}
+        </Typography>
+        <ScatterChart width={700} height={700}>
+          <XAxis
+            dataKey='x'
+            tick={true}
+            hide={true}
+            axisLine={false}
+            type='number'
+            name='X'
+          />
+          <YAxis
+            dataKey='y'
+            tick={true}
+            hide={true}
+            axisLine={false}
+            type='number'
+            name='Y'
+          />
+          <Tooltip content={customToolTip} />
+          <Scatter
+            name='A school'
+            data={data}
+            fill='#32C832'
+            onClick={(waypoint) => {
+              openModal(waypoint);
+            }}
+          />
+        </ScatterChart>
+      </div>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <div className='p-4 rounded-md bg-blackie'>
+          <Typography variant='h6'>
+            Do you want to fly to this waypoint?
+          </Typography>
+          <Button
+            onClick={async () => {
+              await navigateShip({
+                token,
+                shipSymbol: ships.symbol,
+                waypointSymbol: selectedWaypoint.symbol,
+              });
+              closeModal();
+            }}
+          >
+            Navigate
+          </Button>
+        </div>
+      </Modal>
+    </>
   );
 }
